03f24dd66c439887249155dc711fbf74f58d73ab
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-06-10 17:18:26 +0300

    Created ContextMenu

---
+++ src/app/shared/context-menu/context-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component, HostListener, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-context-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './context-menu.component.html',
+  styleUrl: './context-menu.component.scss',
+})
+export class ContextMenuComponent {
+}

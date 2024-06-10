fef84201c82a998ffcb6c90d2b07b15cceabee50
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-06-10 17:20:12 +0300

    Added initial logic to ContextMenu

--- src/app/shared/context-menu/context-menu.component.ts
+++ src/app/shared/context-menu/context-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, Renderer2 } from '@angular/core';
+import { actionMappings } from '../action-mappings';
 
 @Component({
   selector: 'app-context-menu',
@@ -8,4 +9,28 @@ import { Component, HostListener, Renderer2 } from '@angular/core';
   styleUrl: './context-menu.component.scss',
 })
 export class ContextMenuComponent {
+  visible = false;
+  position = { x: 0, y: 0 };
+  target = {} as HTMLElement;
+  availableActions: string[] = [];
+  isNearBottom: boolean = false;
+  viewportHeight = window.innerHeight;
+
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (this.visible) this.close(event);
+  }
+
+  @HostListener('document:contextmenu', ['$event'])
+  onDocumentRightClick(event: MouseEvent) {
+    if (!this.visible) return this.open(event);
+    this.close(event);
+  }
+
+  open(event: MouseEvent) {
+  }
+  close(event: MouseEvent) {
+  }
 }

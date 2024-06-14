dae5d1dbe42c53f8d3882051794f64e6f501d02c
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-06-14 11:57:51 +0300

    Created ResultMenu component

---
+++ src/app/shared/result-menu/result-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-result-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './result-menu.component.html',
+  styleUrl: './result-menu.component.scss',
+})
+export class ResultMenuComponent {
+  constructor() {}
+
+  toggleMenu() {}
+}

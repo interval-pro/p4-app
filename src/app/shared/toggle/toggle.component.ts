d4e323d18fc9f855e349a83ac8789ef83cee4a65
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-06-14 12:26:00 +0300

    Created Toggle component (shared)

---
+++ src/app/shared/toggle/toggle.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  standalone: true,
+  imports: [],
+  templateUrl: './toggle.component.html',
+  styleUrl: './toggle.component.scss',
+})
+export class ToggleComponent {
+  isChecked: boolean = false;
+
+  onToggle() {
+    this.isChecked = !this.isChecked;
+  }
+}

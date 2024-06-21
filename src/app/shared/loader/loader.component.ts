19ee5e11151579a1b65fb36525ca3616dbf63f76
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-06-21 11:38:55 +0300

    Loader receives size as input

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
   imports: [],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.scss'
+  styleUrl: './loader.component.scss',
 })
 export class LoaderComponent {
-
+  @Input() size: string = '2.8rem';
 }

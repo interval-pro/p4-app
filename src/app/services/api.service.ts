08c1d70e02468b9cbee942f4ff324d278bdde3e8
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-06-21 12:30:43 +0300

    Created getLayout and getSection API calls

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -9,23 +9,42 @@ import {
   GeneratedSection,
   Layout,
 } from '../models/api.interfaces';
+import { environment } from '../../environments/environment.development';
+import { ApiEndpoints } from '../constants/api-endpoints.enum';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  companyData = {} as CompanyData;
+  apiUrl = environment.apiUrl;
+  engineType = environment.engineType;
 
   constructor(private http: HttpClient, private fs: FormService) {}
 
-  // Mocking response for development purposes
-  private layoutURL = 'assets/sample-response/layout.json';
-
-  getMockedLayout(): Observable<Layout> {
-    return this.http.get<Layout>(this.layoutURL);
+  getLayout(): Observable<Layout> {
+    return this.http.post<Layout>(
+      this.apiUrl + ApiEndpoints.GENERATE_LAYOUT,
+      { inputs: this.fs.getCompanyData() },
+      { params: { engineType: this.engineType } }
+    );
   }
 
   getSection(section: Partial<FinishedSection>): Observable<GeneratedSection> {
+    return this.http.post<GeneratedSection>(
+      this.apiUrl + ApiEndpoints.GENERATE_SECTION,
+      { inputs: this.fs.getCompanyData(), section },
+      { params: { engineType: this.engineType } }
+    );
+  }
+
+  // Mocking response for development purposes
+  private layoutURL = 'assets/sample-response/layout.json';
+  getMockedLayout(): Observable<Layout> {
+    return this.http.get<Layout>(this.layoutURL);
+  }
+  getMockedSection(
+    section: Partial<FinishedSection>
+  ): Observable<GeneratedSection> {
     let url = '';
 
     for (const sectionURL of sectionsURLs) {
@@ -36,12 +55,16 @@ export class ApiService {
   }
 }
 
+// Mocking sections for development purposes
 const sectionsURLs = [
   {
     sectionId: 'header',
     url: 'assets/sample-response/1716148666288/header.json',
   },
-  { sectionId: 'hero', url: 'assets/sample-response/1716148666288/hero.json' },
+  {
+    sectionId: 'hero',
+    url: 'assets/sample-response/1716148666288/hero.json',
+  },
   {
     sectionId: 'features',
     url: 'assets/sample-response/1716148666288/features.json',

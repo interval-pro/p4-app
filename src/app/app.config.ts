f8c76ee715299c11969f26a654ead374342524b5
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-18 22:46:04 +0300

    Installed toastr and added styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,21 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './interceptors/error.interceptor';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideAnimations(),
+    provideToastr({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-center',
+      // preventDuplicates: true,
+    }),
+  ],
 };

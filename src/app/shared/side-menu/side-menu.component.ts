57eaf0786f055c7c8adad677e6bc1d2e96da832f
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2024-07-20 14:19:39 +0300

    Implemented export functionalities

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { ToggleComponent } from '../toggle/toggle.component';
 import { LoaderComponent } from '../loader/loader.component';
+import { ExportService } from '../../services/export.service';
 
 @Component({
   selector: 'app-side-menu',
@@ -13,9 +14,12 @@ export class SideMenuComponent {
   isOpen: boolean = false;
 
   @Input() isLoadingResultSections: boolean = true;
+  @Input() siteRef = {} as HTMLElement;
   @Output() toggleEdit: EventEmitter<boolean> = new EventEmitter<boolean>(
     false
   );
+
+  constructor(private exportService: ExportService) {}
 
   toggleEditMode(isToggled: boolean) {
     this.toggleEdit.emit(isToggled);
@@ -23,5 +27,24 @@ export class SideMenuComponent {
 
   toggleMenu() {
     if (!this.isLoadingResultSections) this.isOpen = !this.isOpen;
+  }
+
+  exportHtml() {
+    const htmlContent = this.exportService.exportAsHtml(this.siteRef);
+    this.exportService.downloadFile(htmlContent, 'page.html', 'text/html');
+  }
+
+  exportPdf() {
+    this.exportService.exportAsPdf(this.siteRef);
+  }
+
+  exportJson() {
+    const jsonData = { content: this.siteRef.innerHTML };
+    const jsonContent = this.exportService.exportAsJson(jsonData);
+    this.exportService.downloadFile(
+      jsonContent,
+      'page.json',
+      'application/json'
+    );
   }
 }
